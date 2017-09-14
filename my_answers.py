68888be573ca2162b7d2481fdbed1f64862afc8d
Brian Chase <brichase@users.noreply.github.com>
2017-09-14 20:06:36 +0000

    fix build_part2_RNN function to use num_char + rerun notebook

--- my_answers.py
+++ my_answers.py
@@ -69,8 +69,8 @@ def window_transform_text(text, window_size, step_size):
 # a single LSTM hidden layer with softmax activation, categorical_crossentropy loss 
 def build_part2_RNN(window_size, num_chars):
     model = Sequential()
-    model.add(LSTM(200, input_shape = (window_size, 33)))
-    model.add(Dense(33))
+    model.add(LSTM(200, input_shape = (window_size, num_chars)))
+    model.add(Dense(num_chars))
     model.add(Activation('softmax'))
     model.summary()
     return model
