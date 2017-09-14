673c66e3c575541a5d5015158688ccfe913ba8dc
Brian Chase <brichase@users.noreply.github.com>
2017-09-14 06:16:39 +0000

    first pass of notebook and my_answers functions

--- my_answers.py
+++ my_answers.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 from keras.models import Sequential
-from keras.layers import Dense
+from keras.layers import Activation, Dense
 from keras.layers import LSTM
 import keras
 
@@ -13,6 +13,11 @@ def window_transform_series(series, window_size):
     X = []
     y = []
 
+    n_pairs = len(series) - window_size
+    for n_pair in range(n_pairs):
+        X.append(series[n_pair: window_size + n_pair])
+        y.append(series[window_size + n_pair])
+        
     # reshape each 
     X = np.asarray(X)
     X.shape = (np.shape(X)[0:2])
@@ -23,13 +28,26 @@ def window_transform_series(series, window_size):
 
 # TODO: build an RNN to perform regression on our time series input/output data
 def build_part1_RNN(window_size):
-    pass
+    model = Sequential()
+    model.add(LSTM(5, input_shape = (window_size, 1)))
+    model.add(Dense(1))
+    return model
 
 
 ### TODO: return the text input with only ascii lowercase and the punctuation given below included.
 def cleaned_text(text):
     punctuation = ['!', ',', '.', ':', ';', '?']
-
+    ascii_lowercase = 'abcdefghijklmnopqrstuvwxyz'
+    valid_characters = ''.join(punctuation) + ascii_lowercase
+    
+    list_text = list(text)
+    
+    # Replace unwanted characters with a space
+    for i, character in enumerate(text):
+        if character not in valid_characters:
+            list_text[i] = ' '
+    
+    text = ''.join(list_text)
     return text
 
 ### TODO: fill out the function below that transforms the input text and window-size into a set of input/output pairs for use with our RNN model
@@ -37,10 +55,22 @@ def window_transform_text(text, window_size, step_size):
     # containers for input/output pairs
     inputs = []
     outputs = []
-
+    
+    # Create input output pairs
+    number_of_pairs = (len(text) // step_size) - (window_size // step_size)
+    for pair_index in range(number_of_pairs):
+        text_index = pair_index * step_size
+        inputs.append(text[text_index: text_index + window_size])
+        outputs.append(text[text_index + window_size])
+    
     return inputs,outputs
 
 # TODO build the required RNN model: 
 # a single LSTM hidden layer with softmax activation, categorical_crossentropy loss 
 def build_part2_RNN(window_size, num_chars):
-    pass
+    model = Sequential()
+    model.add(LSTM(200, input_shape = (window_size, 33)))
+    model.add(Dense(33))
+    model.add(Activation('softmax'))
+    model.summary()
+    return model
